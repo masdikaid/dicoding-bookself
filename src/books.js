8e602c80eef03e61a7001645bf7ccb8488ea2217
masdika ID <masdikaid@gmail.com>
2021-05-01 17:07:54 +0700

    set books data bases

---
+++ src/books.js
@@ -0,0 +1,14 @@
+// {
+//     "name": string,
+//     "year": number,
+//     "author": string,
+//     "summary": string,
+//     "publisher": string,
+//     "pageCount": number,
+//     "readPage": number,
+//     "reading": boolean
+// }
+
+const books = [];
+
+module.exports = books;
